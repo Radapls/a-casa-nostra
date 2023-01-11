fff1f0e29b5da70ad088ea55023b3f7bdfa15743
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-01-10 21:50:26 -0300

    feat: Add google auth firebase function

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -11,13 +11,56 @@
  * @date Monday, 9th January 2023
  */
 
-import React from 'react'
-import { FcGoogle } from 'react-icons/fc'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
+
 
 export default function OAuth() {
+
+    const navigate = useNavigate();
+
+    async function onGoogleClick() {
+        try {
+            const auth = getAuth();
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user
+
+            // Check if the user is already in the database
+
+            const docRef = doc(db, "users", user.uid)
+
+            const docSnap= await getDoc(docRef)
+
+            if(!docSnap.exists()){
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                })
+            }
+
+            navigate("/")
+            toast.success("User authenticate with success")
+
+
+        } catch (error) {
+          toast.error("Could not authorize with Google")
+        }
+    }
   return (
-    <button className='flex items-center justify-center w-full uppercase bg-red-700 text-white px-7 py-3 text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded-xl'>
-            <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
-            Continue with Google</button>
+    <button
+        type="button"
+        onClick={onGoogleClick}
+        className='flex items-center justify-center w-full uppercase bg-red-700 text-white px-7 py-3 text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded-xl'>
+
+        <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
+        Continue with Google
+    </button>
   )
 }
